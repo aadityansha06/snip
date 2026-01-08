49ac26a9cee922334f8a858df4806ec7fd86f4a0
Aadityansha <96714228+aadityansha06@users.noreply.github.com>
2026-01-08 13:23:38 +0530

    Update snip.c

--- snip.c
+++ snip.c
@@ -1,4 +1,4 @@
-// No AI is used in mkaig of this Project or if used it's mentioned there explicitly ( if you think it's Ai generated due to emoji and formating ) it's your problem then , fuck u 
+
 
 
 
